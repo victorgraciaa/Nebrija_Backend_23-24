8b5333933855ddcc2f0551ea430a493a76204b03
victorgraciaa <victorgrarcos@hotmail.com>
2023-09-21 13:52:33 +0200

    Commit Practica 1

--- P1/ejercicio2.test.ts
+++ P1/ejercicio2.test.ts
@@ -2,6 +2,6 @@ import { multiplos_3_5 } from "./ejercicio2.ts";
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 
 Deno.test("multiplos_3_5", function () {
-    assertEquals(multiplos_3_5(10), [0,3,5,6,9]);
+    assertEquals(multiplos_3_5(10), [0,3,5,6,9,10]);
 }
 );
