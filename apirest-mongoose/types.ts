e1ed5560e11e113f59150e41d6859a30b720e342
victorgraciaa <victorgrarcos@hotmail.com>
2023-10-26 19:55:26 +0200

    Commit API Rest Ejemplo

---
+++ apirest-mongoose/types.ts
@@ -0,0 +1,7 @@
+//type Person es un alias de un objeto en el que se especifica que tiene que tener los campos name, dni, age e id, todos ellos de un tipo concreto
+export type Person = {
+  name: string;
+  dni: string;
+  age: number;
+  id: string;
+};
