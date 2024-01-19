41710544e61e8e2c11deb3cd677b74036b9a7ae8
victorgraciaa <victorgrarcos@hotmail.com>
2024-01-19 17:25:57 +0100

    Commit Ordinaria

--- Ordinaria/main.ts
+++ Ordinaria/main.ts
@@ -6,13 +6,14 @@ import { Contacto } from "./resolvers/Contacto.ts";
 import { typeDefs } from "./gql/schema.ts";
 import mongoose from "mongoose";
 
-const MONGO_URL = Deno.env.get("MONGO_URL");
+//const MONGO_URL = Deno.env.get("MONGO_URL");
+const MONGO_URL = "mongodb+srv://victorgraciaa:nebrija2324@cluster-nebrija-23-24.76u69py.mongodb.net/ordinaria?retryWrites=true&w=majority"
 if (!MONGO_URL) {
   throw new Error("Please provide a MongoDB connection string");
 }
 
 // Connect to MongoDB
-await mongoose.connect(MONGO_URL);
+await mongoose.connect(MONGO_URL);  
 
 console.info("🚀 Connected to MongoDB ");
 
