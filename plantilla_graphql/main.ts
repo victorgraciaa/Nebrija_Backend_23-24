b71e7f6703f8aa9eb81728adbfcabaf7972b879e
victorgraciaa <victorgrarcos@hotmail.com>
2024-06-29 13:37:14 +0200

    Preparando Extraordinaria

--- plantilla_graphql/main.ts
+++ plantilla_graphql/main.ts
@@ -6,8 +6,7 @@ import { Tipo } from "./resolvers/tipo.ts";
 import { typeDefs } from "./gql/schema.ts";
 import mongoose from "mongoose";
 
-// const MONGO_URL = Deno.env.get("MONGO_URL");
-const MONGO_URL = "mongodb+srv://victorgraciaa:nebrija2324@cluster-nebrija-23-24.76u69py.mongodb.net/extraordinaria?retryWrites=true&w=majority"  
+const MONGO_URL = Deno.env.get("MONGO_URL");
 
 if (!MONGO_URL) {
   throw new Error("Please provide a MongoDB connection string");
